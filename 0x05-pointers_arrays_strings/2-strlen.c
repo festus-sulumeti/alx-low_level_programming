aad460af118730128230177283697ef47c59c3c8
festus-sulumeti <sulumetifestus@outlook.com>
2023-03-27 04:31:20 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strlen - return the length of a string
- * Owned By Bwave
+ * Owned By festus Sulumeti
  * @s: char to check
  * Description: this will return the length of a string
  * Return: 0 is success
