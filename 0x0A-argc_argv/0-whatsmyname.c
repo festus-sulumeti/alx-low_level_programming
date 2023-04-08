16e5d4eacb198c79ae1f85cacf44f2a3dfe76bf8
festus-sulumeti <sulumetifestus@outlook.com>
2023-04-08 11:23:41 -0700

    newly made

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of strings containing the arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	(void) argc; /* unused argument 88*/
+	printf("%s\n", argv[0]); /* print the first argument,*/
+	return (0);
+}
