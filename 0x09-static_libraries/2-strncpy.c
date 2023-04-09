2e0dd46ee05e40c0189187bb2882b538c9e4515b
festus-sulumeti <sulumetifestus@outlook.com>
2023-04-09 01:56:55 -0700

    New wave task 0 on 2

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,26 +1,25 @@
 #include "main.h"
 
 /**
- *_strncpy - function that copies a string
- *
- * @src: The source of strings
- * @dest: The destination of the string
- * @n: The length of int
- *
- * Return: pointer to the resulting string dest
+ * _strncpy - copies a string
+ * @dest: Pointer to a character being changed
+ * @src: Pointer to a character being changed
+ * @n: value
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+    int number_1;
 
-	for (i = 0; i < n && *(src + i); i++)
-	{
-		*(dest + i) = *(src + i);
-	}
-	for (; i < n; i++)
-	{
-	*(dest + i) = '\0';
-	}
-	return (dest);
+    for (number_1 = 0; number_1 < n && src[number_1] != '\0'; number_1++)
+    {
+        dest[number_1] = src[number_1];
+    }
 
+    for (; number_1 < n; number_1++)
+    {
+        dest[number_1] = '\0';
+    }
+
+    return (dest);
 }
