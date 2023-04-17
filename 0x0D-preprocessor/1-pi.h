3fbf89ab088e05058c557faee7c2ee4ee5f8a279
festus-sulumeti <sulumetifestus@outlook.com>
2023-04-17 06:54:21 -0700

    a header file that defines a macro named PI as an abbreviation for the token 3.14159265359.

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+* File: pi.h
+* Description: This header file defines a constant named PI,
+*               reps math constant pi 2 high degree of precision.
+* value of const = 3.14159265359.
+Usage: #include "pi.h" in your C source code to use the PI constant.
+*/
+#define PI 3.14159265359
+
+#endif
