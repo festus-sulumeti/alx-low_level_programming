1564ef16c2e50528a22e452aa3761fc39d0d7ad2
festus-sulumeti <sulumetifestus@outlook.com>
2023-04-27 03:03:51 -0700

    Bug fixed

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* print_list - used in checking the code
+* @h: name of the list in
+* Return: the number of nodes.
+*/
+
+size_t print_list(const list_t *h)
+{
+size_t num_nodes = 0;
+
+if (h == NULL)
+return (0);
+
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+num_nodes++;
+h = h->next;
+}
+
+return (num_nodes);
+}
