007fbcf31ad79c8f58dcd55845988d7e10804d0f
festus-sulumeti <sulumetifestus@outlook.com>
2023-04-30 05:18:11 -0700

    Uupdated header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,3 +34,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
+#endif
