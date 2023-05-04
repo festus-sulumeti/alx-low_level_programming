650111c9ba3b35b2d34c4ae29eef0584e2153b50
festus-sulumeti <sulumetifestus@outlook.com>
2023-05-04 06:47:36 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+* binary_to_uint  - convert binary number to unsigned int
+* @b:  pointing to string of 0 and 1 chars
+* Return: converted number or 0 if b is NULL
+*/
+unsigned int binary_to_uint(const char *b)
+{
+
+	unsigned int number = 0;
+	int i;
+
+
+	if (b == NULL)
+		return (0);
+
+
+	for (i = 0; b[i] != '\0'; i++)
+		if (b[i]  != '0' && b[i] != '1')
+			return (0);
+
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		number *= 2;
+		if (b[i] == '1')
+			number += 1;
+	}
+	return (number);
+}
