a8248fdb694caec556e4bc01455943fa3861bf55
festus-sulumeti <sulumetifestus@outlook.com>
2023-07-06 04:33:38 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * get_bit - printing value of a bit at given index
+ * @n: Number checking the value
+ * @index: The index to look for the number
+ * Return: A value at a given index or -1 if an error occurred
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (index > (sizeof(8) * 8))
+		return (-1);
+
+	while (i < index)
+	{
+		n = n >> 1;
+		i++;
+	}
+
+	while (i >= index)
+	{
+		if ((n & 1) == 1)
+			return (1);
+		else
+			return (0);
+	}
+	return (-1);
