548b13b973e4e4754a15b45fb86771d95c3d4093
festus-sulumeti <sulumetifestus@outlook.com>
2023-07-10 03:13:40 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,50 @@
 #include "main.h"
 
-
 /**
-* read_textfile  - reading a text file and prints
-* @filename:  point to a string of 0 and 1 chars
-* @letters : number of letters it should read and print
-* Return: number of letters it could read and print
-*/
+ * read_textfile  - reads a text file and prints
+ * @filename:  pointing to a string of 0 and 1 chars
+ * @letters : the number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int file_d, counter = 0, rd_file;
-char *buffer;
-if (filename == NULL)
-return (0);
-buffer = malloc(letters * sizeof(char));
+	int fd, count = 0, read_file;
+	char *buffer;
 
-if (buffer == NULL)
-return (0);
-
-fd = open(filename, O_RDONLY);
-if (file_d == -1)
-{
-free(buffer);
-return (0);
-}
-
-rd_file = read(file_d, buffer, letters);
-if (rd_file == -1)
-{
-close(file_d);
-free(buffer);
-return (0);
-}
-while (counter < rd_file)
-{
-if (write(STDOUT_FILENO, &buffer[count], 1) == -1)
-{
-close(file_d);
-free(buffer);
-return (0);
-}
-counter++;
-}
-close(file_d);
-free(buffer);
-return (rd_file);
+	/*Check content file*/
+	if (filename == NULL)
+		return (0);
+	buffer = malloc(letters * sizeof(char));
+	/*Check value of buffer*/
+	if (buffer == NULL)
+		return (0);
+	/*Open file*/
+	fd = open(filename, O_RDONLY);
+	if (fd == -1) /*If it fails*/
+	{
+		free(buffer); /*free memory*/
+		return (0);
+	}
+	/*Read file*/
+	read_file = read(fd, buffer, letters);
+	if (read_file == -1) /*If it fails*/
+	{
+		close(fd); /*Close file*/
+		free(buffer);
+		return (0);
+	}
+	while (count < read_file)
+	{
+		if (write(STDOUT_FILENO, &buffer[count], 1) == -1)
+		{
+			close(fd);
+			free(buffer);
+			return (0);
+		}
+		count++;
+	}
+	close(fd); /*Close file*/
+	free(buffer); /*Free memory*/
+	return (read_file);
 }
