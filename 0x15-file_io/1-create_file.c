7b546be8013ff47d286be1a98c8c06adf047ebaf
festus-sulumeti <sulumetifestus@outlook.com>
2023-07-10 03:38:02 -0700

    task done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file  - function that creating a file
  * @filename: name of the file to create
- * @text_content :  NULL terminated string to write to the file
+ * @text_content :  NULL  terminated string to write to the file
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
