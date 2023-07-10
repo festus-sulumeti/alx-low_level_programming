bdf7b97e51f068df28908ba4f78898689784758d
festus-sulumeti <sulumetifestus@outlook.com>
2023-07-10 03:36:03 -0700

    Implement create_file function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,26 +6,22 @@
  * @text_content :  NULL terminated string to write to the file
  * Return: 1 on success, -1 on failure
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	int file_d, write_file;
 
-
 	if (filename == NULL)
 		return (-1);
 
-
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-	if (file_d == -1) /*If it fails*/
+	if (file_d == -1)
 	{
 		return (-1);
 	}
 
 	if (text_content == NULL)
 		return (1);
-
 
 	write_file = write(file_d, text_content, strlen(text_content));
 	close(fd);
