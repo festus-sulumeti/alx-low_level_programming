66309920153417226a04034ce5aea4c74845405f
festus-sulumeti <sulumetifestus@outlook.com>
2023-08-17 06:28:09 -0700

    inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,36 +10,35 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-unsigned int i = 0;
-dlistint_t *node = *h, *new;
+	unsigned int i = 0;
+	dlistint_t *node_prev = *h, *new;
 
-if (*h == NULL)
-return (NULL);
+	if (*h == NULL)
+		return (NULL);
+	/* add node in the front */
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	/* add node in the index */
+	while (node_prev != NULL)
+	{
+		if (i == idx - 1 && node_prev->next != NULL)
+		{
+			new = malloc(sizeof(dlistint_t));
+			if (new == NULL)
+				return (NULL);
+			new->n = n;
+			new->next = node_prev->next;
+			new->prev = node_prev;
+			node_prev->next = new;
+			new->next->prev = new;
+			return (new);
+		}
+		node_prev = node_prev->next;
+		i++;
+	}
 
-if (idx == 0)
-return (add_dnodeint(h, n));
+	if (i == idx)
+		return (add_dnodeint_end(h, n));
 
-while (node != NULL)
-{
-if (i == idx - 1)
-{
-new = malloc(sizeof(dlistint_t));
-if (new == NULL)
-return (NULL);
-new->n = n;
-new->next = node->next;
-new->prev = node;
-if (node->next != NULL)
-node->next->prev = new;
-node->next = new;
-return (new);
-}
-node = node->next;
-i++;
-}
-
-if (i == idx)
-return (add_dnodeint_end(h, n));
-
-return (NULL);
+	return (NULL);
 }
