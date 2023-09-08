355317e42e5c493dc0f514a8e6d4ff0add461668
festus-sulumeti <sulumetifestus@outlook.com>
2023-09-07 23:59:54 -0700

    function that creates a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,23 +6,22 @@
 * @size: number of hash table buckets to create
 * Return: address of new hash table, or NULL if failure
 */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *new_table;
 unsigned long int index;
-
-/* Handle errors */
+/*Handle errors*/
 if (size == 0)
 return (NULL);
 
-/* Allocated memory */
+/*Allocated memory*/
 new_table = malloc(sizeof(hash_table_t));
-
-/* Handle errors */
+/*Handle errors*/
 if (new_table == NULL)
 return (NULL);
 
-/* Allocated memory */
+/*Allocated memory*/
 new_table->array = malloc(sizeof(hash_node_t *) * size);
 
 if (new_table->array == NULL)
@@ -30,11 +29,8 @@ return (NULL);
 
 new_table->size = size;
 
-/* Use a do while loop to initialize the array */
-do {
+for (index = 0; index < size; index++)
 new_table->array[index] = NULL;
-index++;
-} while (index < size);
 
 return (new_table);
 }
